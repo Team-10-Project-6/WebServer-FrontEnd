955dae549f4b7b732ea6345a4a056969b1941d88
[Caleb] <[ccullen8806036@conestogac.on.ca]>
2026-02-12 12:42:09 -0500

    made changes, post request to request images on home page working

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Feed = ({ getAccessTokenSilently }) => {
+
+const Feed = () => {
+    const { getAccessTokenSilently } = useAuth0();
   const [posts, setPosts] = useState([]);
   const [description, setDescription] = useState("");
   const [imageFile, setImageFile] = useState(null);
